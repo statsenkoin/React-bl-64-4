d17991ecb2708dc12386ff7d8f19b4492a1989cd
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-31 22:11:09 +0300

    add persist

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,9 +1,35 @@
-import { todosReducer } from './todoSlice';
-
 import { configureStore } from '@reduxjs/toolkit';
+import { todosReducer } from './todoSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todosReducer);
 
 export const store = configureStore({
   reducer: {
-    todos: todosReducer,
+    todos: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
